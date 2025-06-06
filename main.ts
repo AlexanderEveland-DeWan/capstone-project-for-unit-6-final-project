3d8576577b65eb0494192108bc2ec05e86ac90cc
AlexanderEveland-DeWan <Alexander.eveland-dewan@student.csd509j.net>
2025-06-06 09:31:26 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+//=================================GLOBAL===================================
+
+
+//================================CONSTANTS=================================
+
+
+//================================CLASSES===================================
+
+
+//================================FUNCTIONS==================================
+function createHero(): void {
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . . 
+        . . f f e e e e f 2 f . . . 
+        . f f e e e e f 2 2 2 f . . 
+        . f e e e f f e e e e f . . 
+        . f f f f e e 2 2 2 2 e f . 
+        . f e 2 2 2 f f f f e 2 f . 
+        f f f f f f f e e e f f f . 
+        f f e 4 4 e b f 4 4 e e f . 
+        f e e 4 d 4 1 f d d e f f . 
+        . f e e e 4 d d d d f d d f 
+        . . . f e e 4 e e e f b b f 
+        . . . f 2 2 2 4 d d e b b f 
+        . . f f 4 4 4 e d d e b f . 
+        . . f f f f f f e e f f . . 
+        . . . f f . . . f f f . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(mySprite, 100, 0)
+    mySprite.ay = gravity
+    scene.cameraFollowSprite(mySprite)
+}
+function createTilemap(): void {
+    tiles.setCurrentTilemap(assets.tilemap`crowdRace`)
+}
+//================================EVENT HANDLERS=============================
+game.onUpdate(function () {
+    if (mySprite.vy == 0) {
+        number_of_jumps = 0
+    }
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.vy < 30 || number_of_jumps == 1) {
+        number_of_jumps += 1
+        mySprite.vy = jump_speed
+    }
+})
+
+//================================MAIN=======================================
+createTilemap()
+let gravity = 300
+let mySprite: Sprite = null
+let jump_speed = -175
+let number_of_jumps = 0
+createHero()
---
+++ tilemap.g.ts
@@ -0,0 +1,43 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "crowdRace":
+            case "crowdRace2":return tiles.createTilemap(hex`1000100003020405060708090a0503020809060701010101010101010101010101010101010101010101010101010101010b0b0b0101010101010101010b0b0b01010101010101010b0b0b0101010101010101010101010101010101010101010101010101010b0b0101010b0b01010101010101010101010101010101010b0b0101010101010101010101010101010101010101010101010b0b0b0b01010101010b0b0b010101010101010101010101010101010b0b0b010101010101010b0b0b0101010101010101010101010101010101010101010101010b0b0b0b010101010101010c0101010101010101010101010101010b0b0b0b0b0101010101010101010101`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . 2 2 2 
+. . . . . . . . . 2 2 2 . . . . 
+. . . . 2 2 2 . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . 2 2 . . . 2 2 . . . . . . . 
+. . . . . . . . . . 2 2 . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . 2 2 2 2 . . . . . 2 2 2 
+. . . . . . . . . . . . . . . . 
+2 2 2 . . . . . . . 2 2 2 . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 2 2 2 2 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+2 2 2 2 2 . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.builtin.brick,sprites.builtin.crowd1,sprites.builtin.crowd0,sprites.builtin.crowd2,sprites.builtin.crowd3,sprites.builtin.crowd4,sprites.builtin.crowd5,sprites.builtin.crowd6,sprites.builtin.crowd7,sprites.builtin.crowd8,sprites.vehicle.roadHorizontal,myTiles.tile1], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
